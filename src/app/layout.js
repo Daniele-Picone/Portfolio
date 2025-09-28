38ccf8c430a39f611d85682982625660906c6bc5
Daniele Picone <danielepicone97@gmail.com>
2025-09-28 13:53:13 +0200

    aggiutti informazioni metaDati

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,29 @@ import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 import './context/ThemeContext.css'
 
+
+export const metadata = {
+  title: "Il mio portfolio - Daniele Picone",
+  description: "Portfolio di Daniele Picone : Junior developer fullstack",
+  keywords: ["portfolio", "web developer", "Next.js", "React", "Fullstack"],
+  authors: [{ name: "Daniele Picone" }],
+  openGraph: {
+    title: "Portfolio di Daniele Picone",
+    description: "Junior developer fullstack",
+    url: "https://portfolio-clg8i4qo0b.vercel.app/",
+    siteName: "Portfolio Nome Cognome",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "it_IT",
+    type: "website",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="it">
