f7f0227946ab5c876fa5a9929866a323bc3d0b36
Daniele Picone <danielepicone97@gmail.com>
2025-12-05 12:50:21 +0100

    modifiche css

--- src/data/progject.js
+++ src/data/progject.js
@@ -43,8 +43,10 @@ export const projects = [
     id: 5,
     title: "WineWorld",
     description: "WineWorld è un sito per appassionati di vini ed enologia , sia per esperti che per principianti. Il sito offre conoscenza dalla pianta alla bottiglia , con articoli completi ma alla portata di tutti. Composto di una dashabord per la gestione dei contenuti (articoli, categorie, utenti) e di una parte front-end per la visualizzazione dei contenuti agli utenti finali. C'e la possibilità di registrarsi e autenticarsi ma al momento non è visibile dalla parte front-end per mancanza di conoscenze sulla gestione dei dati sensibili. Ma è pensato per fa usufruire agli utenti della sezione blog per recensire i vini che vengono degustati da loro. In futuro verrà implementartà la possibilità di richiedere al bot il giusto abbinamento cibo-vino in base al piatto che si vuole preparare. ",
-    tech: ["nest.js", "react", "Supabase", "css", "React(QuillEditor ricco per gli articoli)"],
+    tech: ["nest.js", "react", "Supabase", "css", "React (QuillEditor per gli articoli)"],
     github:"https://github.com/Daniele-Picone/wineworld",
-    slug: " https://www.wineworldweb.it/"
+    link: "https://www.wineworldweb.it/",
+    slug: "wineworld",
+    img :"/WineWorld.png"
   }
 ];
